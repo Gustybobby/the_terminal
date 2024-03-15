6ef1bb62ecfa7945c8a415585ffb7ae66b9a8d7c
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-03-15 14:45:54 +0700

    force dynamic

--- app/api/admin/clock/route.ts
+++ app/api/admin/clock/route.ts
@@ -1,6 +1,8 @@
 import { passengerUpdate } from "@/modules/routine";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"
+
 export async function GET(req: NextRequest){
     await passengerUpdate()
     return NextResponse.json("SUCCESS", { status: 200 })

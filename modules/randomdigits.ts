841a3c3d5fdc68642c2ef5577fc3f61aea7c47aa
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-03-25 10:41:43 +0700

    remove user flag capturing

--- modules/randomdigits.ts
+++
@@ -1,9 +0,0 @@
-import { randomInt } from "crypto"
-
-export function randomSixDigits(){
-    let pass = ""
-    for(var i=0;i<6;i++){
-        pass += randomInt(0,10).toString()
-    }
-    return pass
-}

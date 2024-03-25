83110ffb622f8cf5d1e907e21534f1908390506e
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-03-25 10:42:07 +0700

    edit faction details

--- game/faction.ts
+++ game/faction.ts
@@ -5,25 +5,25 @@ export const FACTION_MAP = {
       id: 1,
       name: "School of ICT",
       abbreviation: "ICT",
-      ability_name: "Disable",
-      use: 1,
+      ability_name: "Hack",
+      use: 2,
       duration: -1,
-      duration_factor: 0.05,
+      duration_factor: 0.25,
       type: "Airline Debuff",
       description:
-        "Disabled Debuff จะทำให้เกิดสองอย่าง : 1. หยุดการใช้งานโปรแกรมของ airline หนึ่งไป ทำไม่สามารถใช้งาน device เป็นระยะเวลา 5% ของเฟสนั้น (หยุดการcapture terminal กับ view info ต่างๆ)  2. ทำให้ passengers rate ลดลง 20% ทุกๆ terminal (20 P / 5 Sec ->  16 / 5 Sec)",
+        "Hack จะทำให้เกิดสองอย่าง :\n1. หยุดการใช้งานโปรแกรมของ Airline ไป 1 Airline ทำให้สามารถใช้ Capture Terminal/Use Skill/View Info ได้เป็นระยะเวลา 25% ของเฟสนั้น (หยุดการcapture terminal กับ view info ต่างๆ)\n 2. ทำให้จำนวน Passengers ลดลง 20% ทุกๆ terminal เป็นระยะเวลา 25% ของเฟส",
     },
     MSME: {
       id: 2,
       name: "School of MSME",
       abbreviation: "MSME",
       ability_name: "Logistics Boost",
-      use: 1,
+      use: 2,
       duration: -1,
-      duration_factor: 0.05,
+      duration_factor: 0.25,
       type: "Airline Buff",
       description:
-        "สามารถที่จะ half time interval ของ passenger rate ที่เข้ามาได้เป็นระยะเวลา 5% ของเฟสนั้น Ex. จาก 100 Ps / 20 sec => 100 Ps / 10 Sec for 2 minutes",
+        "สามารถที่จะทำให้ Time Interval ของ Passenger rate ลดลงเหลือ 25% ของเดิมที่เข้ามาได้เป็นระยะเวลา 25% ของเฟสนั้น Ex. จาก 100 Ps / 20 sec => 100 Ps / 10 Sec for 2 minutes",
     },
     BCET: {
       id: 3,
@@ -31,7 +31,7 @@ export const FACTION_MAP = {
       abbreviation: "BCET",
       ability_name: "Lab Explosion",
       use: 1,
-      duration: 5000,
+      duration: -1,
       duration_factor: 1,
       type: "Global Sabotage",
       description:
@@ -48,6 +48,18 @@ export const FACTION_MAP = {
       type: "Passive Buff",
       description:
         "เมื่อครอบครอง terminal ไหนเกินระยะเวลา 50% ของเฟสนั้น  passenger rate ของ Terminal น้้น x3 จนกว่าเวลาจะหมด แต่ว่าเมื่อ  Terminal  ที่ได้ effect นี้ถูกยึดไป ตัวความสามารถจะไม่ทำงานและหยุดการ activate สามารถ apply effect นี้ได้กับ 2 terminal",
+    },
+    MT: {
+      id: 4,
+      name: "School of MT",
+      ability_name: "Decision Making",
+      abbreviation: "MT",
+      use: 2,
+      duration: -1,
+      duration_factor: 1,
+      type: "Terminal Buff/Protection",
+      description:
+        "ธุรกิจมีทั้งข้อดีข้อเสียขอให้เลือกชอยส์ที่ถูกโดยจะมีสองชอยส์เป็น 1. สามารถป้องกัน Terminal ตัวเองได้ 1 ครั้ง เมื่อแพ้ แต่ต้องสูญเสีย 10% passengers rate  2. สามารถ 3x passengers rate กับ terminal ให้เป็น buff ชั่วคราวได้โดยจะมีผลเป็นระยะเวลา 1 นาทีของเฟสนั้นๆ",
     },
     None: {
         id: 999,
@@ -68,16 +80,3 @@ export const FACTIONS = [
     FACTION_MAP["BCET"],
     FACTION_MAP["CET"],
 ]
-
-/**MT: {
-      id: 4,
-      name: "School of MT",
-      ability_name: "Decision Making",
-      abbreviation: "MT",
-      use: 2,
-      duration: 60000,
-      duration_factor: 1,
-      type: "Terminal Buff/Protection",
-      description:
-        "ธุรกิจมีทั้งข้อดีข้อเสียขอให้เลือกชอยส์ที่ถูกโดยจะมีสองชอยส์เป็น 1. สามารถป้องกัน Terminal ตัวเองได้ 1 ครั้ง เมื่อแพ้ แต่ต้องสูญเสีย 10% passengers rate  2. สามารถ 3x passengers rate กับ terminal ให้เป็น buff ชั่วคราวได้โดยจะมีผลเป็นระยะเวลา 1 นาทีของเฟสนั้นๆ",
-    }, */

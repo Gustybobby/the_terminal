01db0ea1bd80b31e8dec6a0e521b6ad8a88bcd42
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-03-27 13:07:47 +0700

    redirect to signin on null id

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
 
 export default withAuth(
     async function middleware(req){
+        if(!req.nextauth.token?.id){
+            return NextResponse.redirect(`/api/auth/signin`)
+        }
     }
 )
 

57bfc966a8be703fafe286297f1d858218406a50
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-03-27 13:00:24 +0700

    add window check

--- components/auth/auth-provider.tsx
+++ components/auth/auth-provider.tsx
@@ -4,6 +4,9 @@ import { Session } from "next-auth";
 import { SessionProvider, signOut } from "next-auth/react";
 
 export default function AuthProvider({ children, session }: { children: React.ReactNode, session: Session | null }){
+    if(typeof window === undefined){
+        return <></>
+    }
     if(session && !session.user.id){
         signOut()
     }
